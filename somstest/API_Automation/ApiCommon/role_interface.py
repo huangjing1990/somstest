4b3e06db712efd958572304cca3c7d3cd122a707
HuangJing <773997817@qq.com>
2021-03-26 16:38:02 +0800

    添加角色管理4条用例

---
+++ somstest/API_Automation/ApiCommon/role_interface.py
@@ -0,0 +1,48 @@
+from Common.Request import *
+from Params.params import *
+
+
+class Role_interface():
+
+    def __init__(self):
+        self.request = Request()
+
+    def add_role(self, domain, cookie):
+        url, data, header = request_data("role_interface", "addRole")
+
+        apiUrl = domain + url
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    def find_role(self, domain, cookie):
+        url, data, header = request_data("role_interface", "findRoleByPage")
+
+        apiUrl = domain + url
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    def edit_role(self, domain, cookie, roleId):
+        url, data, header = request_data("role_interface", "updateRole")
+
+        apiUrl = domain + url
+
+        data["roleId"] = roleId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    def delete_role(self, domain, cookie, roleId):
+        url, data, header = request_data("role_interface", "deleteRole")
+
+        apiUrl = domain + url
+
+        data["roleId"] = roleId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
