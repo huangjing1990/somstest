f5290404d93b5efce6a22c29bb26b7ba2dd14d89
HuangJing <773997817@qq.com>
2021-03-30 17:07:54 +0800

    添加用户管理8条用例，累计20条

---
+++ somstest/API_Automation/ApiCommon/user_interface.py
@@ -0,0 +1,100 @@
+from Common.Request import *
+from Params.params import *
+
+
+class User_interface():
+
+    def __init__(self):
+        self.request = Request()
+
+    # 添加用户
+    def add_user(self, domain, cookie):
+        url, data, header = request_data("user_interface", "addUser")
+
+        apiUrl = domain + url
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 查询用户
+    def find_userByOrg(self, domain, cookie):
+        url, data, header = request_data("user_interface", "findUserByOrg")
+
+        apiUrl = domain + url
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 编辑用户
+    def edit_user(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "updateUser")
+
+        apiUrl = domain + url
+
+        data["userId"] = userId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 查看用户
+    def find_userById(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "findUserById")
+
+        apiUrl = domain + url
+
+        data["userId"] = userId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 用户授权角色
+    def edit_userHasRole(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "updateUserHasRole")
+
+        apiUrl = domain + url
+
+        data["userId"] = userId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 解锁
+    def unLockUser(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "unLockUser")
+
+        apiUrl = domain + url
+
+        data["userId"] = userId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 密码重置
+    def resetPassword(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "resetPassword")
+
+        apiUrl = domain + url
+
+        data["userId"] = userId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 删除用户
+    def delete_user(self, domain, cookie, userId):
+        url, data, header = request_data("user_interface", "deleteUserById")
+
+        apiUrl = domain + url
+
+        data["userIds"] = userId + ","
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
---
+++ somstest/API_Automation/TestCase/test_user.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021-3-30
+# @Author  : huangjing
+# @File    : test_role.py
+# 在研发与技术推广中心下，创建python用户，授权武汉研发专用的角色，后删除
+
+import time
+from TestCase.initEnv import *
+import unittest
+from ApiCommon.user_interface import *
+from ApiCommon.Login_interface import *
+from Params.params import *
+
+
+class TestRole(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.initEvn = initEvn()
+        cls.req_url = cls.initEvn.host
+        cls.login = Login_interface()
+        cls.user = User_interface()
+        cls.g = globals()
+        cls.g["Cookie"] = initEvn().get_userCookie()
+
+        LOG.info('测试用例开始执行')
+
+    def tearDown(self):
+        LOG.info('测试用例执行完毕')
+
+    @logger("添加用户")
+    def test_adduser(self):
+        """
+            用例描述：添加用户
+        """
+        response = self.user.add_user(self.req_url, self.g["Cookie"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("查询用户")
+    def test_findUserByOrg(self):
+        """
+            用例描述：查询用户
+        """
+        response = self.user.find_userByOrg(self.req_url, self.g["Cookie"])
+        self.g["userId"] = response["body"]["rows"][0]["userId"]
+        print(self.g["userId"])
+        assert self.initEvn.test.assert_in_text(response['body'], "python")
+
+    @logger("编辑用户")
+    def test_edituser(self):
+        """
+            用例描述：编辑用户
+        """
+        response = self.user.edit_user(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("查看用户")
+    def test_findUserById(self):
+        """
+            用例描述：查看用户
+        """
+        response = self.user.find_userById(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_in_text(response['body'], "python")
+
+    @logger("用户授权角色")
+    def test_userHasRole(self):
+        """
+            用例描述：用户授权角色
+        """
+        response = self.user.edit_userHasRole(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("解锁")
+    def test_unLockUser(self):
+        """
+            用例描述：解锁
+        """
+        response = self.user.unLockUser(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("密码重置")
+    def test_resetPassword(self):
+        """
+            用例描述：密码重置
+        """
+        response = self.user.resetPassword(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("删除用户")
+    def test_deleteUser(self):
+        """
+            用例描述：删除用户
+        """
+        response = self.user.delete_user(self.req_url, self.g["Cookie"], self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
