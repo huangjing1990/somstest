8aec1c84517075dff3cfd73b729972c68fbf84c0
HuangJing <773997817@qq.com>
2022-02-10 14:11:04 +0800

    2022-2-10 11:00 罗军新增【人工计划】11条用例，累计147条。

--- somstest/API_Automation/ApiCommon/operationPlan_interface.py
+++ somstest/API_Automation/ApiCommon/operationPlan_interface.py
@@ -7,87 +7,146 @@ class OperationPlan_interface():
     def __init__(self):
         self.request = Request()
 
-    def add_operationPlan(self, domain, cookie, month=None):
+    # 添加人工计划
+    def add_operationPlan(self, domain, cookie, roadSectionIds=None, month=None):
         url, data, header = request_data("operationPlan_interface", "addOperationPlan")
 
         apiUrl = domain + url
 
-        data["orgName"] = month
+        data["roadSectionIds"] = roadSectionIds
+        data["month"] = month
 
         response = self.request.post_request(apiUrl, data, header, cookie)
 
         return response
 
+    # 查询人工计划
+    def find_operationPlan(self, domain, cookie, monthStart=None, monthEnd=None, roadSectionIds=None):
+        url, data, header = request_data("operationPlan_interface", "findOperationPlan")
 
-    # def find_org(self, domain, cookie):
-    #     url, data, header = request_data("org_interface", "findOrgByTree")
-    #
-    #     apiUrl = domain + url
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def edit_org(self, domain, cookie, orgId, orgName):
-    #     url, data, header = request_data("org_interface", "editOrg")
-    #
-    #     apiUrl = domain + url
-    #
-    #     data["orgId"] = orgId
-    #     data["orgName"] = orgName
-    #     data["shortName"] = orgName
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def delete_org(self, domain, cookie, orgId):
-    #     url, data, header = request_data("org_interface", "deleteOrg")
-    #
-    #     apiUrl = domain + url
-    #
-    #     data["orgId"] = orgId
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def add_team(self, domain, cookie):
-    #     url, data, header = request_data("org_interface", "addTeam")
-    #
-    #     apiUrl = domain + url
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def find_team(self, domain, cookie):
-    #     url, data, header = request_data("org_interface", "findTeamById")
-    #
-    #     apiUrl = domain + url
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def edit_team(self, domain, cookie, teamId):
-    #     url, data, header = request_data("org_interface", "editTeam")
-    #
-    #     apiUrl = domain + url
-    #
-    #     data["orgId"] = teamId
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
-    #
-    # def delete_team(self, domain, cookie, teamId):
-    #     url, data, header = request_data("org_interface", "deleteTeam")
-    #
-    #     apiUrl = domain + url
-    #
-    #     data["orgId"] = teamId
-    #
-    #     response = self.request.post_request(apiUrl, data, header, cookie)
-    #
-    #     return response
+        apiUrl = domain + url
+
+        data["monthStart"] = monthStart
+        data["monthEnd"] = monthEnd
+        data["roadSectionIds"] = roadSectionIds
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 编辑人工计划
+    def edit_operationPlan(self, domain, cookie, operationPlanId=None, month=None):
+        url, data, header = request_data("operationPlan_interface", "editOperationPlan")
+
+        apiUrl = domain + url
+
+        data["operationPlanId"] = operationPlanId
+        data["month"] = month
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 新建计划明细
+    def add_PlanDetail(self, domain, cookie, date=None, operationPlanId=None, usersId=None):
+        url, data, header = request_data("operationPlan_interface", "addPlanDetail")
+
+        apiUrl = domain + url
+
+        data["date"] = date
+        data["operationPlanId"] = operationPlanId
+        data["usersId"] = usersId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 顺延计划明细
+    def postponePlanDetail(self, domain, cookie, operationPlanId=None, roadSectionId=None, date=None, taskdays=None,
+                           userdays=None):
+        url, data, header = request_data("operationPlan_interface", "postponePlanDetail")
+
+        apiUrl = domain + url
+
+        data["operationPlanId"] = operationPlanId
+        data["roadSectionId"] = roadSectionId
+        data["date"] = date
+        data["taskdays"] = taskdays
+        data["userdays"] = userdays
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 提交审核
+    def submitOperationPlan(self, domain, cookie, operationPlanIds=None):
+        url, data, header = request_data("operationPlan_interface", "submitOperationPlan")
+
+        apiUrl = domain + url
+
+        data["operationPlanIds"] = operationPlanIds
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 查询审核记录
+    def findOperationAuditing(self, domain, cookie, monthStart=None, monthEnd=None):
+        url, data, header = request_data("operationPlan_interface", "findOperationAuditing")
+
+        apiUrl = domain + url
+
+        data["monthStart"] = monthStart
+        data["monthEnd"] = monthEnd
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 审核通过
+    def createAuditing(self, domain, cookie, operationPlanIds=None):
+        url, data, header = request_data("operationPlan_interface", "createAuditing")
+
+        apiUrl = domain + url
+
+        data["operationPlanIds"] = operationPlanIds
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 下发计划
+    def issueOperationPlan(self, domain, cookie, operationPlanIds=None):
+        url, data, header = request_data("operationPlan_interface", "issueOperationPlan")
+
+        apiUrl = domain + url
+
+        data["operationPlanIds"] = operationPlanIds
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 顺延计划
+    def postponePlan(self, domain, cookie, operationPlanIds=None):
+        url, data, header = request_data("operationPlan_interface", "postponePlan")
+
+        apiUrl = domain + url
+
+        data["operationPlanIds"] = operationPlanIds
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
+
+    # 删除计划
+    def deleteOperationPlan(self, domain, cookie, operationPlanId=None):
+        url, data, header = request_data("operationPlan_interface", "deleteOperationPlan")
+
+        apiUrl = domain + url
+
+        data["operationPlanId"] = operationPlanId
+
+        response = self.request.post_request(apiUrl, data, header, cookie)
+
+        return response
--- somstest/API_Automation/TestCase/test_operationPlan.py
+++ somstest/API_Automation/TestCase/test_operationPlan.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2022-1-24
-# @Author  : 罗军
+# @Time    : 2022-2-8
+# @Author  : luojun
 # @File    : test_operationPlan.py
-# 使用009269的账号登录，进入人工计划，道路选择【黄山大道中段-01-路段】，在该道路下添加人工计划。
+# 肖海波009269登录系统，进行人工计划的增删改查。部门id=1053（大竹林项目部），项目ID=5，青竹东路下自动创建路段。班组id：1061渝美班组，自动创建人员
+# 本模块用例每月最后一天执行会存在问题，计划是当月的，而明细是下个月的。
 
 import time
+import calendar
 import random
+from datetime import datetime
+from dateutils import relativedelta
 from TestCase.initEnv import *
 import unittest
+from ApiCommon.operationPlan_interface import *
 from ApiCommon.roadSection_interface import *
 from ApiCommon.Login_interface import *
+from ApiCommon.user_interface import *
 from Params.params import *
 
 
@@ -20,7 +26,9 @@ class TestOperationPlan(unittest.TestCase):
         cls.initEvn = initEvn()
         cls.req_url = cls.initEvn.host
         cls.login = Login_interface()
-        cls.roadSection = RoadSection_interface()
+        cls.OperationPlan = OperationPlan_interface()
+        cls.RoadSection = RoadSection_interface()
+        cls.user = User_interface()
         cls.g = globals()
         cls.g["Cookie"] = initEvn().get_userCookie()
 
@@ -34,41 +42,149 @@ class TestOperationPlan(unittest.TestCase):
         """
             人工计划：添加人工计划
         """
-        self.g["add_operationPlan"] = "python青竹东路-" + str(random.randint(100, 999)) + "-人工计划"
-        response = self.roadSection.add_roadSection(self.req_url, self.g["Cookie"],
-                                                    roadSectionName=self.g["roadSectionName"])
+
+        self.g["roadSectionName"] = "python路段" + str(random.randint(100, 999))
+        self.RoadSection.add_roadSection(self.req_url, self.g["Cookie"],
+                                         roadSectionName=self.g["roadSectionName"])
+
+        response2 = self.RoadSection.find_roadSection(self.req_url, self.g["Cookie"])
+        pythonRoad = response2["body"]["rows"]
+        for i in range(0, len(pythonRoad)):
+            if pythonRoad[i]["roadSectionName"] == self.g["roadSectionName"]:
+                self.g["roadSectionIds"] = pythonRoad[i]["roadSectionId"]
+
+        month = time.strftime("%Y-%m")
+        response3 = self.OperationPlan.add_operationPlan(self.req_url, self.g["Cookie"],
+                                                         roadSectionIds=self.g["roadSectionIds"], month=month)
+        assert self.initEvn.test.assert_body(response3['body'], 'resultCode', 1)
+
+    @logger("查询人工计划")
+    def test_findOperationPlan(self):
+        """
+            人工计划：查询人工计划
+        """
+        monthStart = time.strftime("%Y-%m-01 00:00:00")
+        end = time.strftime("%Y-%m-01 23:59:59")
+        end2 = datetime.strptime(end, "%Y-%m-%d %H:%M:%S") + relativedelta(months=+1)
+        monthEnd = datetime.strftime(end2, "%Y-%m-%d %H:%M:%S")
+        response = self.OperationPlan.find_operationPlan(self.req_url, self.g["Cookie"], monthStart=monthStart,
+                                                         monthEnd=monthEnd, roadSectionIds=self.g["roadSectionIds"])
+        pythonPlan = response["body"]["rows"]
+        for i in range(0, len(pythonPlan)):
+            if pythonPlan[i]["roadSectionId"] == self.g["roadSectionIds"]:
+                self.g["operationPlanId"] = pythonPlan[i]["operationPlanId"]
+                print(self.g["operationPlanId"])
+        assert self.initEvn.test.assert_in_text(response['body'], self.g["roadSectionName"])
+
+    @logger("编辑人工计划")
+    def test_editOperationPlan(self):
+        """
+            人工计划：编辑人工计划
+        """
+        month = time.strftime("%Y-%m")
+        response = self.OperationPlan.edit_operationPlan(self.req_url, self.g["Cookie"],
+                                                         operationPlanId=self.g["operationPlanId"],
+                                                         month=month)
         assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
 
+    @logger("新建计划明细")
+    def test_addOperationPlanDetail(self):
+        """
+            人工计划：新建计划明细
+        """
 
+        date1 = time.strftime("%Y-%m-%d")
+        date2 = datetime.strptime(date1, "%Y-%m-%d") + relativedelta(days=+1)
+        date = datetime.strftime(date2, "%Y-%m-%d")
 
-    # @logger("查询人工计划")
-    # def test_findRoadSection(self):
-    #     """
-    #         人工计划：查询人工计划
-    #     """
-    #     response = self.roadSection.find_roadSection(self.req_url, self.g["Cookie"])
-    #     pythonRoadSection = response["body"]["rows"]
-    #     for i in range(0, len(pythonRoadSection)):
-    #         if pythonRoadSection[i]["roadSectionName"] == self.g["roadSectionName"]:
-    #             self.g["roadSectionId"] = pythonRoadSection[i]["roadSectionId"]
-    #     print(self.g["roadSectionId"])
-    #     assert self.initEvn.test.assert_in_text(response['body'], self.g["roadSectionName"])
-    #
-    # @logger("编辑人工计划")
-    # def test_editRoadSection(self):
-    #     """
-    #         人工计划：编辑人工计划
-    #     """
-    #     self.g["roadSectionName"] = "编辑" + self.g["roadSectionName"]
-    #     response = self.roadSection.edit_roadSection(self.req_url, self.g["Cookie"],
-    #                                                  roadSectionId=self.g["roadSectionId"],
-    #                                                  roadSectionName=self.g["roadSectionName"])
-    #     assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
-    #
-    # @logger("删除人工计划")
-    # def test_deleteRoadSection(self):
-    #     """
-    #         人工计划：删除人工计划
-    #     """
-    #     response = self.roadSection.delete_roadSection(self.req_url, self.g["Cookie"], self.g["roadSectionId"])
-    #     assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+        self.g["employeeId"] = str(random.randint(700000, 800000))
+        self.g["username"] = "python用户" + str(random.randint(100, 1000))
+        response1 = self.user.add_user(self.req_url, self.g["Cookie"], employeeId=self.g["employeeId"], orgId="1061",
+                                       username=self.g["username"])
+        # 查询用户
+        response2 = self.user.findUserByPage(self.req_url, self.g["Cookie"], employeeId=self.g["employeeId"],
+                                             orgdetpIds="[1061]")
+        self.g["userId"] = response2["body"]["rows"][0]["userId"]
+        print(self.g["userId"])
+
+        response = self.OperationPlan.add_PlanDetail(self.req_url, self.g["Cookie"],
+                                                     operationPlanId=self.g["operationPlanId"], date=date,
+                                                     usersId=self.g["userId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("顺延计划明细")
+    def test_postponeOperationPlanDetail(self):
+        """
+            人工计划：顺延明细
+        """
+        date1 = time.strftime("%Y-%m-%d")
+        date2 = datetime.strptime(date1, "%Y-%m-%d") + relativedelta(days=+1)
+        date = datetime.strftime(date2, "%Y-%m-%d")
+        day1 = datetime.today()
+        day2 = calendar.monthrange(day1.year, day1.month)
+        days = day2[1] - day1.day - 1
+        response = self.OperationPlan.postponePlanDetail(self.req_url, self.g["Cookie"],
+                                                         operationPlanId=self.g["operationPlanId"],
+                                                         roadSectionId=self.g["roadSectionIds"], date=date,
+                                                         taskdays=days,
+                                                         userdays=days)
+        assert self.initEvn.test.assert_in_text(response['body'], self.g["username"])
+
+    @logger("提交审核")
+    def test_submitCleanPlan(self):
+        """
+            人工计划：提交审核
+        """
+        self.g["operationPlanIds"] = str(self.g["operationPlanId"]) + ","
+        response = self.OperationPlan.submitOperationPlan(self.req_url, self.g["Cookie"],
+                                                          operationPlanIds=self.g["operationPlanIds"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("查询审核记录")
+    def test_findCleanAuditing(self):
+        """
+            人工计划：查询审核记录
+        """
+        monthStart = time.strftime("%Y-%m-01 00:00:00")
+        end = time.strftime("%Y-%m-01 23:59:59")
+        end2 = datetime.strptime(end, "%Y-%m-%d %H:%M:%S") + relativedelta(months=+1)
+        monthEnd = datetime.strftime(end2, "%Y-%m-%d %H:%M:%S")
+        response = self.OperationPlan.findOperationAuditing(self.req_url, self.g["Cookie"], monthStart=monthStart,
+                                                            monthEnd=monthEnd)
+        assert self.initEvn.test.assert_in_text(response['body'], str(self.g["operationPlanId"]))
+
+    @logger("审核通过")
+    def test_createAuditing(self):
+        """
+            人工计划：审核通过
+        """
+        response = self.OperationPlan.createAuditing(self.req_url, self.g["Cookie"],
+                                                     operationPlanIds=self.g["operationPlanId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("下发计划")
+    def test_issueOperationPlan(self):
+        """
+            人工计划：下发计划
+        """
+        response = self.OperationPlan.issueOperationPlan(self.req_url, self.g["Cookie"],
+                                                         operationPlanIds=self.g["operationPlanId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("顺延计划")
+    def test_postponePlan(self):
+        """
+            人工计划：顺延计划
+        """
+        response = self.OperationPlan.postponePlan(self.req_url, self.g["Cookie"],
+                                                   operationPlanIds=self.g["operationPlanId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 1)
+
+    @logger("删除人工计划")
+    def test_deleteOperationPlan(self):
+        """
+            人工计划：删除人工计划
+        """
+        response = self.OperationPlan.deleteOperationPlan(self.req_url, self.g["Cookie"],
+                                                          operationPlanId=self.g["operationPlanId"])
+        assert self.initEvn.test.assert_body(response['body'], 'resultCode', 2)
